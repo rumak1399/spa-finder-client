6f1ce29dc6df303da80af7a37854b41a1d389529
shovonroy173 <shovonroy2003@gmail.com>
2025-05-11 15:45:44 +0600

    debug 6

--- src/lib/clientPromise.js
+++ src/lib/clientPromise.js
@@ -55,9 +55,9 @@
 // lib/mongodb.js
 const { MongoClient, ServerApiVersion } = require("mongodb");
 
-if (!process.env.MONGODB_URI) {
-  throw new Error('Invalid/Missing environment variable: "MONGODB_URI"');
-}
+// if (!process.env.MONGODB_URI) {
+//   throw new Error('Invalid/Missing environment variable: "MONGODB_URI"');
+// }
 
 const uri = process.env.MONGODB_URI;
 const options = {
